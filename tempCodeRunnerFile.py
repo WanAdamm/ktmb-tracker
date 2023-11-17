b6eb6b26de99d0b60d13584a56ef8c866512e669
WanAdamm <136413386+WanAdamm@users.noreply.github.com>
2023-11-17 19:03:37 +0800

    Delete tempCodeRunnerFile.py
    delete useless local file

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-# Extract and print vehicle position information
-vehicle_positions = [MessageToDict(entity.vehicle) for entity in feed.entity]
-print(f'Total vehicles: {len(vehicle_positions)}')
-df = pd.json_normalize(vehicle_positions)
